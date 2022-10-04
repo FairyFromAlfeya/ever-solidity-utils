c0fc9e34954ec7a6747eea9cecc075579e1059cb
Александр Кунеков <fairyfromalfeya@gmail.com>
2022-10-05 01:00:44 +0200

    feat: upgradable by request interfaces

---
+++ contracts/upgrade/interfaces/IUpgradableByRequest.sol
@@ -0,0 +1,30 @@
+pragma ton-solidity >= 0.57.1;
+
+/// @title Upgradable by Request Interface
+/// @notice Interface for contract upgrade by request implementation
+interface IUpgradableByRequest {
+    /// @dev Emits when the contract's upgrader was changed
+    event UpgraderChanged(
+        address current,
+        address previous
+    );
+
+    /// @notice Get the current contract's upgrader
+    /// @return address Upgrader's address
+    function getUpgrader() external view responsible returns (address);
+
+    /// @dev Request contract upgrade from upgrader
+    /// @param _remainingGasTo Gas recipient
+    function requestUpgrade(optional(address) _remainingGasTo) external view;
+
+    /// @notice Upgrades contract's code
+    /// @dev Only the upgrader can perform
+    /// @param _version New version of the contract
+    /// @param _code Contract's new code
+    /// @param _remainingGasTo Gas recipient
+    function upgrade(
+        uint32 _version,
+        TvmCell _code,
+        address _remainingGasTo
+    ) external;
+}
---
+++ contracts/upgrade/interfaces/IUpgrader.sol
@@ -0,0 +1,14 @@
+pragma ton-solidity >= 0.57.1;
+
+/// @title Upgrader Interface
+/// @notice Interface for upgrader-contract implementation
+interface IUpgrader {
+    /// @notice Upgrades child contract's code
+    /// @dev Only the child contract can call
+    /// @param _currentVersion Version of the contract which request upgrade
+    /// @param _remainingGasTo Gas recipient
+    function requestUpgrade(
+        uint32 _currentVersion,
+        address _remainingGasTo
+    ) external view;
+}
