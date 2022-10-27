99edc16889b88201e8d6a55eb942864fcbfcd52d
Александр Кунеков <fairyfromalfeya@gmail.com>
2022-10-27 07:27:51 +0200

    refactor: dex contracts

--- contracts/dex/DexPlatform.sol
+++ contracts/dex/DexPlatform.sol
@@ -1,26 +1,26 @@
-pragma ton-solidity >= 0.57.0;
+pragma ever-solidity >= 0.63.0;
 
-pragma AbiHeader time;
-pragma AbiHeader expire;
-pragma AbiHeader pubkey;
-
+/// @author Alexander Hramcov
 /// @title DEX Platform
-/// @notice Implements base functions for factory contract
+/// @notice Minimized DEX platform contract for addresses deriving
 /// @dev Use this contract only for tvm.buildStateInit
 contract DexPlatform {
     /// @dev Address of the DEX root
     address static root;
 
-    /// @dev Type of the instance (account or pair)
+    /// @dev Type of the instance (account or pair). See DexPlatformTypes
     uint8 static type_id;
 
-    /// @dev Deploy params
+    /// @dev Specific params which depend on the contract's type
+    /// @dev For account: cell(address). Where an address is owner of the account
+    /// @dev For pool: cell(leftAddress, rightAddress). Where leftAddress < rightAddress
+    ///      and addresses are the pool's token roots
     TvmCell static params;
 
-    /// @dev Platform constructor
-    /// @param _code Instance code
-    /// @param _version Code's version
-    /// @param _vault DEX vault address
+    /// @dev Platform constructor. Upgrades platform to the _code under the hood
+    /// @param _code Code of the instance for upgrade to
+    /// @param _version Code's version of the instance
+    /// @param _vault Vault's address of the DEX
     /// @param _remainingGasTo Gas recipient
     constructor(
         TvmCell _code,
---
+++ contracts/dex/libraries/DexPlatformTypes.sol
@@ -0,0 +1,9 @@
+pragma ever-solidity >= 0.63.0;
+
+/// @author Alexander Hramcov
+/// @title DEX Platform Types
+/// @notice Different contract types of DEX for deploying via DexPlatform
+library DexPlatformTypes {
+    uint8 constant ACCOUNT = 1;
+    uint8 constant POOL = 2;
+}
